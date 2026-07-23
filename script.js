e544a21fb54aa691498236454d5681206815769e
NovaChain <aytekinm099@gmail.com>
2026-07-23 18:23:59 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("NovaChain Mini App Started");
+
+    const btn = document.getElementById("connect");
+
+    if(btn){
+        btn.onclick = () => {
+            alert("TON Wallet integration will be added soon.");
+        };
+    }
+});
